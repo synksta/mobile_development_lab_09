1b6945d5b2a4aa47b0f696ea2b1fec7e42d1a286
synksta <contriturbamentum@gmail.com>
2024-12-12 14:07:14 +0700

    Movies - add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -45,4 +46,17 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    implementation("androidx.room:room-runtime:2.6.1") // Основная библиотека Room
+    kapt("androidx.room:room-compiler:2.6.1") // Компилятор для аннотаций
+    implementation("androidx.room:room-ktx:2.6.1") // KTX для удобства работы с Room
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+//    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+//    implementation(libs.androidx.fragment.ktx)
+//
+//    implementation(libs.retrofit)
+//    implementation(libs.converter.gson.v290)
+//    implementation(libs.gson)
 }
