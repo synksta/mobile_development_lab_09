d5733aa63943646367406730251d764be1e2dec6
synksta <contriturbamentum@gmail.com>
2024-12-13 09:39:28 +0700

    Movies - add ImageView extension to fetch image from url

---
+++ app/src/main/java/com/example/mobile_development_lab_09/extenstion/ImageViewExtension.kt
@@ -0,0 +1,14 @@
+// ImageViewExtensions.kt
+package com.example.mobile_development_lab_09.extenstion
+
+import android.widget.ImageView
+import com.example.mobile_development_lab_09.R
+import com.squareup.picasso.Picasso
+
+fun ImageView.fetchImage(url: String?) {
+    Picasso.get()
+        .load(url)
+        .placeholder(R.drawable.image_placeholder)
+        .error(R.drawable.image_placeholder_error)
+        .into(this)
+}
